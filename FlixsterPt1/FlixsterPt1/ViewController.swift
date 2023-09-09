7a3d1b6ca3ecd513d9fcb1695ba465e36f0f9816
jaydess21 <98706895+jaydess21@users.noreply.github.com>
2023-09-09 17:52:40 -0400

    Started project

---
+++ FlixsterPt1/FlixsterPt1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlixsterPt1
+//
+//  Created by Jayde S on 9/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
