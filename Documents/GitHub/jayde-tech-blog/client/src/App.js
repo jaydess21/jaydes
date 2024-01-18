d8c6f90ee5231b7caf967269e652cb8146d7af00
jaydess21 <98706895+jaydess21@users.noreply.github.com>
2024-01-18 11:42:02 -0500

    Inital Commit

---
+++ Documents/GitHub/jayde-tech-blog/client/src/App.js
@@ -0,0 +1,61 @@
+import './App.css';
+
+function App() {
+  return (
+ <main> 
+  <header> <a href = "" className = "logo">TechExpress Blog</a>
+  <nav>
+<a href = "">Login</a>
+<a href = "">Register</a>
+  </nav>
+   </header>
+   <div className= "post">
+    <div className = "image">
+    <img src = "https://news.mit.edu/sites/default/files/styles/news_article__image_gallery/public/images/202401/FIND_0.png?itok=Onony6AR" 
+    alt = ""/>
+      </div>
+    <div className = "text">
+    <h2>How to navigate in college</h2>
+    <p>Navigating college successfully involves a combination of academic and personal strategies. First, prioritize 
+      time management by creating a schedule that includes class times, study sessions, and extracurricular activities. 
+      Utilize campus resources such as libraries, tutoring services, and academic advisors to enhance your academic performance. 
+      Foster connections with professors and peers, as networking can
+      lead to valuable opportunities both academically and professionally. Lastly, take care of your well-being by maintaining a 
+      healthy balance between academics, social life, and self-care to ensure a fulfilling college experience.</p>
+</div> 
+</div>
+<div className= "post">
+<div className = "image">
+    <img src = "https://news.mit.edu/sites/default/files/styles/news_article__image_gallery/public/images/202401/FIND_0.png?itok=Onony6AR" 
+    alt = ""/>
+      </div>
+     <div className = "text">
+    <h2>How to navigate in college</h2>
+    <p>Navigating college successfully involves a combination of academic and personal strategies. First, prioritize 
+      time management by creating a schedule that includes class times, study sessions, and extracurricular activities. 
+      Utilize campus resources such as libraries, tutoring services, and academic advisors to enhance your academic performance. 
+      Foster connections with professors and peers, as networking can
+      lead to valuable opportunities both academically and professionally. Lastly, take care of your well-being by maintaining a 
+      healthy balance between academics, social life, and self-care to ensure a fulfilling college experience.</p>
+</div>
+</div>
+<div className= "post">
+<div className = "image">
+    <img src = "https://news.mit.edu/sites/default/files/styles/news_article__image_gallery/public/images/202401/FIND_0.png?itok=Onony6AR" 
+    alt = ""/>
+      </div>
+      <div className = "text">
+    <h2>How to navigate in college</h2>
+    <p>Navigating college successfully involves a combination of academic and personal strategies. First, prioritize 
+      time management by creating a schedule that includes class times, study sessions, and extracurricular activities. 
+      Utilize campus resources such as libraries, tutoring services, and academic advisors to enhance your academic performance. 
+      Foster connections with professors and peers, as networking can
+      lead to valuable opportunities both academically and professionally. Lastly, take care of your well-being by maintaining a 
+      healthy balance between academics, social life, and self-care to ensure a fulfilling college experience.</p>
+</div>
+</div>
+ </main>
+  );
+}
+
+export default App;
